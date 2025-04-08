aecb0f50713736d4523d32bd23e28623be423bb8
IBangedMyToaster <danzer38+github@gmail.com>
2025-04-08 23:11:23 +0200

    no message

--- Rockstar.Snapmatic/SnapJSON.cs
+++ Rockstar.Snapmatic/SnapJSON.cs
@@ -2,38 +2,26 @@
 {
     public class SnapJSON
     {
-        public SnapLocation loc { get; set; }
-        public string area { get; set; }
+        public SnapLocation loc { get; set; } = null!;
+        public string area { get; set; } = null!;
         public long street { get; set; }
-        public string nm { get; set; }
-        public string rds { get; set; }
+        public string nm { get; set; } = null!;
+        public string rds { get; set; } = null!;
         public int scr { get; set; }
-        public string sid { get; set; }
+        public string sid { get; set; } = null!;
         public long crewid { get; set; }
-        public string mid { get; set; }
-        public string mode { get; set; }
+        public string mid { get; set; } = null!;
+        public string mode { get; set; } = null!;
         public bool meme { get; set; }
         public bool mug { get; set; }
         public long uid { get; set; }
-        public SnapTime time { get; set; }
+        public SnapTime time { get; set; } = null!;
         public long creat { get; set; }
         public bool slf { get; set; }
         public bool drctr { get; set; }
 
-        public SnapLocation SnapLocation
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        public SnapLocation SnapLocation { get; set; } = null!;
 
-        public SnapTime SnapTime
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        public SnapTime SnapTime { get; set; } = null!;
     }
 }
